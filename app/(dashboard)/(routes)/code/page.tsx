fbc7debb3a5ff45c64cd838ba0603a7e901c3974
mateshwari <ganesh.medidi@think.design>
2024-02-25 14:13:10 +0530

    interface updated

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -8,7 +8,6 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 import ReactMarkdown from "react-markdown";
 import { useRouter } from "next/navigation";
-import { ChatCompletionRequestMessage } from "openai";
 
 import { BotAvatar } from "@/components/bot-avatar";
 import { Heading } from "@/components/heading";
@@ -23,6 +22,13 @@ import { Empty } from "@/components/ui/empty";
 import { useProModal } from "@/hooks/use-pro-modal";
 
 import { formSchema } from "./constants";
+
+
+interface ChatCompletionRequestMessage {
+  role: 'user' | 'assistant' | 'system';
+  content: string;
+  name?: string;
+}
 
 const CodePage = () => {
   const router = useRouter();
--- constants.ts
+++ constants.ts
@@ -39,3 +39,4 @@ export const tools = [
     href: '/code',
   },
 ];
+
