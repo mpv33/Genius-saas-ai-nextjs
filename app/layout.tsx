f0ffa0db55c05cc5c14237ca46463741c5b1dc10
mateshwari <ganesh.medidi@think.design>
2024-02-27 20:30:32 +0530

    added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,36 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-
-import { ToasterProvider } from '@/components/toaster-provider'
-import { ModalProvider } from '@/components/modal-provider'
-import { CrispProvider } from '@/components/crisp-provider'
-
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ToasterProvider } from '@/components/toaster-provider';
+import { ModalProvider } from '@/components/modal-provider';
+import { CrispProvider } from '@/components/crisp-provider';
+import './globals.css';
 
 const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Genius',
-  description: 'AI Platform',
-}
+  title: 'Genius OpenAI',
+  description: 'Experience the transformative power of Genius OpenAI on our website, where image generation, chat assistance, and code generation converge to unlock unparalleled possibilities.'
+};
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta charSet="UTF-8" />
+          <meta property="og:type" content='website' />
+          <meta property="og:url" content='https://genius-openai.vercel.app/' />
+          <meta property="og:title" content='Genius OpenAI' />
+          <meta property="og:description" content='Experience the transformative power of Genius OpenAI on our website, where image generation, chat assistance, and code generation converge to unlock unparalleled possibilities.' />
+          <meta property="og:image" content='/preview.png' />
+          <meta property="og:image:alt" content='Genius Dashboard' />
+        </head>
         <CrispProvider />
         <body className={font.className}>
           <ToasterProvider />
@@ -31,5 +39,5 @@ export default async function RootLayout({
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
