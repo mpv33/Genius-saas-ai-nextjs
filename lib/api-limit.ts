c715a7926cb90cbc11cd2d606727ce7f3f1055a5
mateshwari <ganesh.medidi@think.design>
2024-03-02 03:05:34 +0530

    removed user first name

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -8,7 +8,7 @@ connectDB();
 
 export const incrementApiLimit = async () => {
   const { userId } = auth();
-  const user = await currentUser(); // Get current user
+  //const user = await currentUser(); // Get current user
 
   if (!userId) {
     return;
@@ -19,18 +19,18 @@ export const incrementApiLimit = async () => {
 
     let userApiLimit = await UserApiLimit.findOne({ userId });
 
-    if (user) { // Add null check for user
+   // if (user) { // Add null check for user
       if (userApiLimit) {
         userApiLimit.count += 1;
       } else {
         userApiLimit = new UserApiLimit({
-           userId, count: 1,
-           firstName:user?.firstName || ''
+           userId, count: 1
+         //  firstName:user?.firstName || ''
           }); // Include user's first name
       }
 
       await userApiLimit.save();
-    }
+   // }
   } catch (error) {
     console.error(error);
   }
